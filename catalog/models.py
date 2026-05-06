468160099b17ff43b6e35f26eaa0577fe3b9a10f
mashagulko <gulko.masha06@gmail.com>
2026-05-06 23:48:51 +0300

    update

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Profession(models.Model):
+    title = models.CharField("Название профессии", max_length=200)
+    description = models.TextField("Описание", null=True, blank=True)
+    salary = models.IntegerField("Зарплата", null=True, blank=True)
+    link = models.URLField("Ссылка", null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Профессия"
+        verbose_name_plural = "Профессии"
--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from django.contrib.auth.models import User
+from catalog.models import Profession
 
-# Create your models here.
+
+class Test(models.Model):
+    title = models.CharField("Название теста", max_length=200)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Тест"
+        verbose_name_plural = "Тесты"
+
+
+class Question(models.Model):
+    test = models.ForeignKey(
+        Test,
+        on_delete=models.CASCADE,
+        verbose_name="Тест"
+    )
+    text = models.TextField("Вопрос")
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = "Вопрос"
+        verbose_name_plural = "Вопросы"
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+        verbose_name="Вопрос"
+    )
+    text = models.CharField("Ответ", max_length=200)
+    profession = models.ForeignKey(
+        Profession,
+        on_delete=models.CASCADE,
+        verbose_name="Профессия"
+    )
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = "Ответ"
+        verbose_name_plural = "Ответы"
+
+
+class TestResult(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        verbose_name="Пользователь"
+    )
+    profession = models.ForeignKey(
+        Profession,
+        on_delete=models.CASCADE,
+        verbose_name="Профессия"
+    )
+    score = models.IntegerField("Баллы")
+    created_at = models.DateTimeField("Дата прохождения", auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.profession}"
+
+    class Meta:
+        verbose_name = "Результат теста"
+        verbose_name_plural = "Результаты тестов"
